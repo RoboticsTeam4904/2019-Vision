4a914c5cec3fd94ad70c22d15761a308acce347b
Casey Manning <kcgrind@gmail.com>
2019-01-07 16:37:56 -0800

    fixed code

--- alignmentcode/main.py
+++ alignmentcode/main.py
@@ -1,8 +1,19 @@
 import cv2
 import WebCam
-image = WebCam.getImage()
+from gripCode import Grip
 
-cv2.imshow(name, image)
-	key = cv2.waitKey(20)
-	if key == 27:
-		sys.exit()
+WebCam.set(exposure = 1)
+
+def rgbThreshold(input, red, green, blue):
+    out = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)
+    return cv2.inRange(out, (red[0], green[0], blue[0]),  (red[1], green[1], blue[1]))
+
+while True:
+    image = WebCam.getImage()
+    thresh = rgbThreshold(image,(150,255),(150,255),(220,255))
+    # mask = cv2.bitwise_and(image, input, mask=mask)
+    cv2.imshow('aa', thresh)
+
+    key = cv2.waitKey(20)
+    if key == 27:
+    	sys.exit()
