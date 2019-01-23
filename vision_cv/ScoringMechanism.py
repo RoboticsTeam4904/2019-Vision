4ef5b5554be1c23ed95b4c05ff3fa0e9cc41b92f
Yash <yasnara04@gmail.com>
2019-01-22 19:37:07 -0800

    Fast working contour detection using opencv and scoring metric

--- vision_cv/ScoringMechanism.py
+++ vision_cv/ScoringMechanism.py
@@ -3,10 +3,13 @@ import math
 def dist2d(p1, p2):
     return math.sqrt(abs(p2[0]-p1[0])**2 + abs(p2[1]-p1[1])**2)
 
-def score(bb):
-    weight_ratio = 1
-    weight_area = 1
+def score(bb, weight_ratio, weight_area, weight_parallelogram_infunc, 
+    weight_parallelogram_outfunc, weight_rotation_angle_infunc, weight_rotation_angle_outfunc, threshold):
+    
+    # weight_rect_infunc = 1 #Unnecessary for now
+    # weight_rect_outfunc = 1 #Unnecessary for now
     total_score = 0 
+
     
     bb = sorted(bb, key=lambda x: x[1])[::-1]
     top = bb.pop(0)
@@ -17,37 +20,40 @@ def score(bb):
     bb = sorted(bb, key=lambda x: x[0])[::-1]
     right = bb.pop(0)
 
-    print(top, bottom, left, right)
+    points = (top, right, bottom, left)
     if(len(set([tuple(top), tuple(bottom), tuple(left), tuple(right)])) < 4):
-        return ()
+        return -float("inf")
 
-    width = dist2d(top, right)
-    height = dist2d(top, left)
-    print(width, height)
+    dimension = (dist2d(top, left) , dist2d(top, right)) #Height, width tuple
 
-    total_score += score_side_ratio(height, width, weight_ratio)
-    total_score += score_area_ratio(height, width, top, bottom, left, right, weight_area)
+    total_score += score_side_ratio(dimension) * weight_ratio
+    total_score += score_area_ratio(dimension, points) * weight_area
+    total_score += scoring_parallelogram(points, weight_parallelogram_infunc) * weight_parallelogram_outfunc
+    total_score += scoring_rotation_angle(right, bottom, weight_rotation_angle_infunc) * weight_rotation_angle_outfunc
+    # total_score += scoring_parallelogram(points, weight_rect_infunc) * weight_rectangle_outfunc #Unnecessary for now
+    return total_score
 
-    return (top, bottom, left, right)
-
-def score_side_ratio(h, w, weight):
+def score_side_ratio(dimension):
+    h,w = dimension
     target_ratio = 2.75
     # 5.5 / 2 = 2.75
-    return weight*(1/(((h/w)-target_ratio)**2 + 1)) # Witch of Agnesi curve.
+    return (1/(((h/w)-target_ratio)**2 + 1)) # Witch of Agnesi curve.
 
-def score_area_ratio(h, w, t, b, l, r, weight):
+def score_area_ratio(dimension, points):
+    t,r,b,l = points
+    h,w = dimension
     target_ratio = 0.75
-
     area_slanted = h*w
     area_straight = abs(t[1]-b[1])*abs(r[1]-l[1])
-    score_area_ratio = weight*(1/(abs((area_slanted/area_straight)-target_ratio)**2 + 1))
+    score_area_ratio = (1/(abs((area_slanted/(area_straight+0.0001))-target_ratio)**2 + 1))
+    return score_area_ratio
 
 def scoring_rectangle(points, weight):
     angles = []
     for i in range (-2,len(points)-2):
         angles.append(findInsideAngle(points[i], points[i+1], points[i+2]))
     if None in angles:
-        print angles
+        print(angles)
         return False
     angles = [elem/math.pi * 180 for elem in angles]
     angles = [(90-elem)**2 for elem in angles] #Squared error of angles
@@ -59,13 +65,20 @@ def scoring_parallelogram(points, weight):
     for i in range (-2,len(points)-2): #Negative indices index from the back of the list
         angles.append(findInsideAngle(points[i], points[i+1], points[i+2]))
     if None in angles:
-        print angles
+        print(angles)
         return False
     angles = [elem/math.pi * 180 for elem in angles]
     sumTotal = 0
     for i in range(2):
         sumTotal += (angles[i+2]-angles[i])**2
     return -sumTotal/(sumTotal+weight) +1
+
+def scoring_rotation_angle(right, bottom, weight):
+    slopePoints = slope(right, bottom)
+    angle = math.atan(slopePoints)
+    angle = angle
+    num = min((14.5-angle)**2, (14.5+angle-90)**2)
+    return -num/(num+weight)+1
 
 def findInsideAngle(first, main, third):
     
@@ -84,12 +97,5 @@ def findInsideAngle(first, main, third):
 def slope(point1, point2):
     point1 = [float(elem) for elem in point1]
     point2 = [float(elem) for elem in point2]
-    m = (point2[1]-point1[1])/(point2[0]-point1[0])
+    m = (point2[1]-point1[1])/(point2[0]-point1[0]+0.0001)
     return m
-
-def findOutside(right, bottom, weight):
-    slopePoints = slope(right, bottom)
-    angle = math.atan(slopePoints)
-    angle = 90-angle
-    num = (14.5-angle)**2
-    return -num/(num+weight)+1
