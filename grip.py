76c0ba5f8abe9dde0ac30c075322d4c394f1a7ac
lauwong <lauwong@nuevaschool.org>
2019-01-24 16:48:02 -0800

    Updated GRIP to read contours instead of lines, better erosion and filtering

--- grip.py
+++ grip.py
@@ -12,66 +12,76 @@ class GripPipeline:
         """initializes all values to presets or None if need to be set
         """
 
-        self.__hsv_threshold_hue = [0.0, 39.42275042444821]
-        self.__hsv_threshold_saturation = [0.0, 42.86078098471987]
-        self.__hsv_threshold_value = [110.07194244604317, 255.0]
+        self.__rgb_threshold_red = [222.4370503597122, 255.0]
+        self.__rgb_threshold_green = [222.43705035971226, 255.0]
+        self.__rgb_threshold_blue = [222.43705035971223, 255.0]
 
-        self.hsv_threshold_output = None
+        self.rgb_threshold_output = None
 
-        self.__cv_erode_src = self.hsv_threshold_output
+        self.__cv_erode_src = self.rgb_threshold_output
         self.__cv_erode_kernel = None
         self.__cv_erode_anchor = (-1, -1)
-        self.__cv_erode_iterations = 4.0
+        self.__cv_erode_iterations = 10.0
         self.__cv_erode_bordertype = cv2.BORDER_CONSTANT
         self.__cv_erode_bordervalue = (-1)
 
         self.cv_erode_output = None
 
+        self.__find_contours_input = self.cv_erode_output
+        self.__find_contours_external_only = False
 
-        self.__mask_mask = self.cv_erode_output
+        self.find_contours_output = None
 
-        self.mask_output = None
+        self.__filter_contours_contours = self.find_contours_output
+        self.__filter_contours_min_area = 1000.0
+        self.__filter_contours_min_perimeter = 500.0
+        self.__filter_contours_min_width = 0
+        self.__filter_contours_max_width = 1000
+        self.__filter_contours_min_height = 0
+        self.__filter_contours_max_height = 1000
+        self.__filter_contours_solidity = [66.54676258992806, 100]
+        self.__filter_contours_max_vertices = 1000000
+        self.__filter_contours_min_vertices = 0
+        self.__filter_contours_min_ratio = 0
+        self.__filter_contours_max_ratio = 1000
 
-        self.__find_lines_input = self.mask_output
-
-        self.find_lines_output = None
+        self.filter_contours_output = None
 
 
     def process(self, source0):
         """
         Runs the pipeline and sets all outputs to new values.
         """
-        # Step HSV_Threshold0:
-        self.__hsv_threshold_input = source0
-        (self.hsv_threshold_output) = self.__hsv_threshold(self.__hsv_threshold_input, self.__hsv_threshold_hue, self.__hsv_threshold_saturation, self.__hsv_threshold_value)
+        # Step RGB_Threshold0:
+        self.__rgb_threshold_input = source0
+        (self.rgb_threshold_output) = self.__rgb_threshold(self.__rgb_threshold_input, self.__rgb_threshold_red, self.__rgb_threshold_green, self.__rgb_threshold_blue)
 
         # Step CV_erode0:
-        self.__cv_erode_src = self.hsv_threshold_output
+        self.__cv_erode_src = self.rgb_threshold_output
         (self.cv_erode_output) = self.__cv_erode(self.__cv_erode_src, self.__cv_erode_kernel, self.__cv_erode_anchor, self.__cv_erode_iterations, self.__cv_erode_bordertype, self.__cv_erode_bordervalue)
 
-        # Step Mask0:
-        self.__mask_input = source0
-        self.__mask_mask = self.cv_erode_output
-        (self.mask_output) = self.__mask(self.__mask_input, self.__mask_mask)
+        # Step Find_Contours0:
+        self.__find_contours_input = self.cv_erode_output
+        (self.find_contours_output) = self.__find_contours(self.__find_contours_input, self.__find_contours_external_only)
 
-        # Step Find_Lines0:
-        self.__find_lines_input = self.mask_output
-        (self.find_lines_output) = self.__find_lines(self.__find_lines_input)
+        # Step Filter_Contours0:
+        self.__filter_contours_contours = self.find_contours_output
+        (self.filter_contours_output) = self.__filter_contours(self.__filter_contours_contours, self.__filter_contours_min_area, self.__filter_contours_min_perimeter, self.__filter_contours_min_width, self.__filter_contours_max_width, self.__filter_contours_min_height, self.__filter_contours_max_height, self.__filter_contours_solidity, self.__filter_contours_max_vertices, self.__filter_contours_min_vertices, self.__filter_contours_min_ratio, self.__filter_contours_max_ratio)
 
 
     @staticmethod
-    def __hsv_threshold(input, hue, sat, val):
-        """Segment an image based on hue, saturation, and value ranges.
+    def __rgb_threshold(input, red, green, blue):
+        """Segment an image based on color ranges.
         Args:
             input: A BGR numpy.ndarray.
-            hue: A list of two numbers the are the min and max hue.
-            sat: A list of two numbers the are the min and max saturation.
-            lum: A list of two numbers the are the min and max value.
+            red: A list of two numbers the are the min and max red.
+            green: A list of two numbers the are the min and max green.
+            blue: A list of two numbers the are the min and max blue.
         Returns:
             A black and white numpy.ndarray.
         """
-        out = cv2.cvtColor(input, cv2.COLOR_BGR2HSV)
-        return cv2.inRange(out, (hue[0], sat[0], val[0]),  (hue[1], sat[1], val[1]))
+        out = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)
+        return cv2.inRange(out, (red[0], green[0], blue[0]),  (red[1], green[1], blue[1]))
 
     @staticmethod
     def __cv_erode(src, kernel, anchor, iterations, border_type, border_value):
@@ -89,49 +99,65 @@ class GripPipeline:
                             borderType = border_type, borderValue = border_value)
 
     @staticmethod
-    def __mask(input, mask):
-        """Filter out an area of an image using a binary mask.
-        Args:
-            input: A three channel numpy.ndarray.
-            mask: A black and white numpy.ndarray.
-        Returns:
-            A three channel numpy.ndarray.
-        """
-        return cv2.bitwise_and(input, input, mask=mask)
-
-    class Line:
-
-        def __init__(self, x1, y1, x2, y2):
-            self.x1 = x1
-            self.y1 = y1
-            self.x2 = x2
-            self.y2 = y2
-
-        def length(self):
-            return numpy.sqrt(pow(self.x2 - self.x1, 2) + pow(self.y2 - self.y1, 2))
-
-        def angle(self):
-            return math.degrees(math.atan2(self.y2 - self.y1, self.x2 - self.x1))
-    @staticmethod
-    def __find_lines(input):
-        """Finds all line segments in an image.
+    def __find_contours(input, external_only):
+        """Sets the values of pixels in a binary image to their distance to the nearest black pixel.
         Args:
             input: A numpy.ndarray.
-        Returns:
-            A filtered list of Lines.
+            external_only: A boolean. If true only external contours are found.
+        Return:
+            A list of numpy.ndarray where each one represents a contour.
         """
-        detector = cv2.createLineSegmentDetector()
-        if (len(input.shape) == 2 or input.shape[2] == 1):
-            lines = detector.detect(input)
+        if(external_only):
+            mode = cv2.RETR_EXTERNAL
         else:
-            tmp = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
-            lines = detector.detect(tmp)
+            mode = cv2.RETR_LIST
+        method = cv2.CHAIN_APPROX_SIMPLE
+        im2, contours, hierarchy =cv2.findContours(input, mode=mode, method=method)
+        return contours
+
+    @staticmethod
+    def __filter_contours(input_contours, min_area, min_perimeter, min_width, max_width,
+                        min_height, max_height, solidity, max_vertex_count, min_vertex_count,
+                        min_ratio, max_ratio):
+        """Filters out contours that do not meet certain criteria.
+        Args:
+            input_contours: Contours as a list of numpy.ndarray.
+            min_area: The minimum area of a contour that will be kept.
+            min_perimeter: The minimum perimeter of a contour that will be kept.
+            min_width: Minimum width of a contour.
+            max_width: MaxWidth maximum width.
+            min_height: Minimum height.
+            max_height: Maximimum height.
+            solidity: The minimum and maximum solidity of a contour.
+            min_vertex_count: Minimum vertex Count of the contours.
+            max_vertex_count: Maximum vertex Count.
+            min_ratio: Minimum ratio of width to height.
+            max_ratio: Maximum ratio of width to height.
+        Returns:
+            Contours as a list of numpy.ndarray.
+        """
         output = []
-        if len(lines) != 0:
-            for i in range(1, len(lines[0])):
-                tmp = GripPipeline.Line(lines[0][i, 0][0], lines[0][i, 0][1],
-                                lines[0][i, 0][2], lines[0][i, 0][3])
-                output.append(tmp)
+        for contour in input_contours:
+            x,y,w,h = cv2.boundingRect(contour)
+            if (w < min_width or w > max_width):
+                continue
+            if (h < min_height or h > max_height):
+                continue
+            area = cv2.contourArea(contour)
+            if (area < min_area):
+                continue
+            if (cv2.arcLength(contour, True) < min_perimeter):
+                continue
+            hull = cv2.convexHull(contour)
+            solid = 100 * area / cv2.contourArea(hull)
+            if (solid < solidity[0] or solid > solidity[1]):
+                continue
+            if (len(contour) < min_vertex_count or len(contour) > max_vertex_count):
+                continue
+            ratio = (float)(w) / h
+            if (ratio < min_ratio or ratio > max_ratio):
+                continue
+            output.append(contour)
         return output
 
 
