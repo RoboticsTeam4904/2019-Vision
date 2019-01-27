42f309a91405adbcffd87f7b3ebb33e64b790afd
Yash <yasnara04@gmail.com>
2019-01-27 14:09:01 -0800

    Intermediate commit

--- vision_cv/PairFinding.py
+++ vision_cv/PairFinding.py
@@ -1,29 +1,33 @@
 import ScoringMetric 
-
+import math
 def right_or_left(box):
     top = box[0]
     right = box[1]
     bottom = box[2]
     left = box[3]
     first = min([right, left], key = lambda t:t[1])
+    print(right==first)
     final = (bottom[0]+.1,bottom[1])
     angle = ScoringMetric.findInsideAngle(first, bottom, final)
-    if abs(175.5-angle)<abs(14.5-angle):
+    print(angle/math.pi*180)
+    if abs(175.5-angle/math.pi*180)<abs(14.5-angle/math.pi*180):
         return 'l'
     else:
         return 'r'
 
 def pair_finding(boxes):
     pairs = []
+    boxes.sort(key=lambda x: x[3][0])
     if len(boxes)==1:
         return None
     list_rotations = []
     for i in boxes:
         list_rotations.append(right_or_left(i))
+    print(list_rotations)
     for i in range(0,len(list_rotations)-1):
         if list_rotations[i]=='l' and list_rotations[i+1]=='r':
             pairs.append((boxes[i],boxes[i+1]))
-    if len(pairs)==0:
-        return None
-    else:
+    if len(pairs)>0:
         return pairs
+    else:
+        return None
