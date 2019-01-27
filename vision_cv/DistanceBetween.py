608e90f50d4cd6dc4bb160142187423f300cf54c
bcho04 <brandon.cho@gmail.com>
2019-01-27 15:12:58 -0800

    Modify formatting for filenames, variables, and loops

--- vision_cv/DistanceBetween.py
+++ vision_cv/DistanceBetween.py
@@ -8,11 +8,11 @@ import cv2
  
 def midpoint(ptA, ptB):
 	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
- 
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
-	help="path to the input image")
+				help="path to the input image")
 ap.add_argument("-w", "--width", type=float, required=True,
-	help="width of the left-most object in the image (in inches)")
+				help="width of the left-most object in the image (in inches)")
 args = vars(ap.parse_args())
