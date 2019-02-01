75910c11ba66b13d74f6f8eae66ffff3319a34de
Evan Steirman <evastei@nus11172-10-evastei.nueva.local>
2019-01-31 16:53:47 -0800

    switched from RGB to HSL, added more photos, changed the function to use filtered contours instead of the raw ones

--- rungrip.py
+++ rungrip.py
@@ -1,55 +1,35 @@
 import grip
 import cv2
 
-#image = 'IMG_8968.jpg' #threshold 150-250
-image = 'IMG_7133.jpg' #bad picture
-#image = 'IMG_6140.jpg' #threshold 210-250
-#image = 'IMG_6289.jpg' #bad picture
-#image = 'IMG_3417.jpg' #threshold 130-250
-#image = 'IMG_9615.jpg' #threshold 180-215
-#image = 'IMG_9850.jpg' #threshold 100-250
+#image = 'IMG_0006.jpg'
+#image = 'IMG_1794.jpg'
+#image = 'IMG_2065.jpg'
+#image = 'IMG_3417.jpg'
+#image = 'IMG_5222.jpg'
+#image = 'IMG_6140.jpg'
+#image = 'IMG_6289.jpg'
+#image = 'IMG_7133.jpg'
+#image = 'IMG_8539.jpg'
+#image = 'IMG_8968.jpg'
+#image = 'IMG_9615.jpg'
+#image = 'IMG_9801.jpg'
+#image = 'IMG_9850.jpg'
 img = cv2.imread(image)
 cv2.imshow('unprocessed', img)
 
-
-gripfunc = grip.GripPipelineTwo()
+gripfunc = grip.GripPipeline()
 gripfunc.process(img)
-contours = gripfunc.find_contours_output
-
+contours = gripfunc.filter_contours_output
 
 cv2.drawContours(img, contours, -1, (25,25,245), 10)
 
 cnt = contours[0]
-
-#M = cv2.moments(cnt)
-# print(M)
-#cx = int(M['m10']/M['m00'])
-#cy = int(M['m01']/M['m00'])
-#cv2.circle(img, (cx,cy), 10, (70,20,200), -1)
 
 rows,cols = img.shape[:2]
 [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
 lefty = int((-x*vy/vx) + y)
 righty = int(((cols-x)*vy/vx)+y)
 img = cv2.line(img,(cols-1,righty),(0,lefty),(0,0,0),8)
-
-# print(x)
-# print(y)
-# print(vx)
-# print(vy)
-# print(lefty)
-# print(righty)
-
-
-# gripfunc = grip.GripPipeline()
-# gripfunc.process(img)
-# output = gripfunc.find_lines_output
-
-
-# lineThickness = 8
-# for line in output:
-# 	cv2.line(img, (line.x1, line.y1), (line.x2, line.y2), (70,20,200), lineThickness)
-
 
 cv2.imshow('processed', img)
 cv2.waitKey(0)
