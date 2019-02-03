269c3361d880bfdeb94f02fa193eec574c8dc9ba
akshar <aksramk@nuevaschool.org>
2019-02-03 15:06:44 -0800

    Cleaned code

---
+++ vision_cv/GetContours.py
@@ -0,0 +1,57 @@
+import cv2
+import HSVThreshold
+import main
+import numpy as np
+
+def detect(c):
+    # Initialize the shape name and approximate the contour
+    x, y, w, h = cv2.boundingRect(c)
+    if (w < main.MIN_WIDTH or w > main.MAX_WIDTH):
+        return False
+    if (h < main.MIN_HEIGHT or h > main.MAX_HEIGHT):
+        return False
+    area = cv2.contourArea(c)
+    if (area < main.MIN_AREA):
+        return False
+    if (cv2.arcLength(c, True) < main.MIN_PERIMETER):
+        return False
+    hull = cv2.convexHull(c)
+    solid = 100 * area / cv2.contourArea(hull)
+    if (solid < main.SOLIDITY[0] or solid > main.SOLIDITY[1]):
+        return False
+    ratio = (float)(w) / h
+    if (ratio < main.MIN_RATIO or ratio > main.MAX_RATIO):
+        return False
+    return True
+
+def findContours(img):
+
+    thresholder = HSVThreshold.HSVPipeline()
+    thresh = thresholder.process(img)
+    mask = cv2.bitwise_and(img, img, mask=thresh)
+    mode = cv2.RETR_LIST
+    method = cv2.CHAIN_APPROX_SIMPLE
+    contours = [] 
+
+    if(cv2.__version__[0] == "4"):
+        contours, hierarchy = cv2.findContours(thresh, mode, method) # im2 only in cv2 v3.x
+    if(cv2.__version__[0] == "3"):
+        im2, contours, hierarchy = cv2.findContours(thresh, mode, method) # im2 only in cv2 v3.x
+    if(cv2.__version__[0] == "2"):
+        contours, hierarchy = cv2.findContours(thresh, mode, method) # im2 only in cv2 v3.x
+
+    return thresh, contours, mask
+
+
+def filterContours(contours):
+    return [x for x in contours if (cv2.contourArea(x) > 0 and detect(x))]
+
+
+def getContours(img):
+    thresh, contours, mask = findContours(img)
+    if len(contours) < 1:
+            print "No Contours"
+            return [[-1]], [[-1]], [[-1]]
+    else:
+        contours = filterContours(contours)
+    return thresh, contours, mask
---
+++ vision_cv/ImageAnalysis.py
@@ -0,0 +1,62 @@
+import cv2
+import GetContours
+import numpy as np
+import ScoringMetric
+import config
+import Printing
+import main
+import DrawImage
+
+def imageAnalysis(img):
+    n=0
+    thresh, contours, mask = GetContours.getContours(img)
+    if thresh[0][0] ==-1:
+        return None
+
+    contours, mask, boxes, box_scores = getBoxes(contours, mask, img)
+
+    # Now we draw boxes;
+    box_scores = sorted(box_scores, key=lambda x: x[1])[::-1]
+    box_scores_filtered = []
+    for elem in box_scores:
+        if(elem[1] >= main.MIN_THRESHOLD):
+            box_scores_filtered.append(elem)
+        else:
+            break
+
+    box_scores = box_scores_filtered # Final scores for each contour
+
+    # DrawImage.drawPairs(boxes)
+    DrawImage.drawBoxes(box_scores, mask)
+
+    # GetDistanceAngle.distanceAngleAnalysis(boxes)
+    cv2.drawContours(mask, contours, 0, (0,0,255), 2) # BGR, so this is red.
+
+    print(len(box_scores))
+
+    if not config.LiveImage:
+        cv2.imshow("Threshold", thresh)
+        cv2.imshow("Image", img)
+        cv2.imshow("Mask", mask)
+        cv2.waitKey(0)
+    else:
+        Printing.save(Printing.save(img, name="TEST" + str(n//10)))
+        n+=1
+
+def getBoxes(contours, mask, img):
+    box_scores = []
+    boxes = []
+    for i in range(len(contours)):
+        rect = cv2.minAreaRect(contours[i])
+        if(cv2.__version__[0] == "2"):
+            box = cv2.cv.BoxPoints(rect)
+        else:
+            box = cv2.boxPoints(rect)
+        
+        box = np.int0(box)
+
+        points, contour_score = ScoringMetric.score(box, contours[i], img, main.WEIGHTS)
+        boxes.append(points) #Array with all of the boxes with the format (t, r, b, l) for pair finding 
+        box_scores.append((box, contour_score))
+        cv2.drawContours(mask,[box],0,(0,255,0),2)
+    return contours, mask, boxes, box_scores
