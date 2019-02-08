d134538e7ef0675f1dea6c30cfaa45fbd37f207e
yashn35 <yasnara04@gmail.com>
2019-02-07 17:07:43 -0800

    Inter commit

--- vision_cv/GetDistanceAngle.py
+++ vision_cv/GetDistanceAngle.py
@@ -1,7 +1,7 @@
 import math
 import cv2
 
-def getAngle(box, perfectWidth=3.3133853031, perfectHeight=5.8255720302): 
+def getAngle(box, perfectWidth=3.3133853031, perfectHeight=5.8255720302, fieldOfVision=1.229, imageWidth=640): #Perfect width and height describe the pixel width and height when at the vertical offset and looking straight at the tape. This facilitates angle calculations.
     perfectRatio = perfectWidth/perfectHeight # Perfect ratio is the perfect width divided by the perfect height
     top = box[0]
     right = box[1]
@@ -9,14 +9,19 @@ def getAngle(box, perfectWidth=3.3133853031, perfectHeight=5.8255720302):
     left = box[3]
     width = right[0]-left[0]
     height = top[1]-bottom[1]
+    P2 = imageWidth/2
+    averageX = (left[0]+right[0])/2
+    P1 = averageX - 320
+
     try:
-        theta = math.asin(width/(height*perfectRatio))
+        beta = math.asin(width/(height*perfectRatio))
+        theta = math.atan(P1 * math.tan(fieldOfVision/2)/P2)
     except:
         return width, height
-    return theta, theta/math.pi*180
+    alpha = math.pi -beta + theta
+    return alpha, alpha/math.pi*180
 
-def getDistance(box, sensorHeight, focalLength=3.67, knownHeightMillimeters=139.7, imageHeight=640): 
-
+def getDistance(box, sensorHeight, focalLength=3.67, knownHeightMillimeters=139.7, imageHeight=480):
     #knownHeightMilimeters is the height of the vision tape in millimeters, which stays constant in the code
     #Focal Length 3.67 mm
     top = box[0]
@@ -30,7 +35,7 @@ def getDistance(box, sensorHeight, focalLength=3.67, knownHeightMillimeters=139.
     except: 
         return "CAN'T GET DISTANCE"
 
-def getSensorHeight(box, distanceMillimeters, focalLength=3.67, knownHeightMillimeters=139.7, imageHeight = 640):
+def getSensorHeight(box, distanceMillimeters, focalLength=3.67, knownHeightMillimeters=139.7, imageHeight=480):
     top = box[0]
     right = box[1]
     bottom = box[1]
--- vision_cv/PairFinding.py
+++ vision_cv/PairFinding.py
@@ -1,13 +1,61 @@
-import ScoringMetric 
+from ScoringMetric import findInsideAngle
+import ScoringMetric
 import math
+
+HEIGHT_THRESHOLD = 0.01 # delta pixels # TODO: Tune Threshold
+ANGLE_THRESHOLD = 0.1 # delta radians
+
+"""
+What we want to do:
+Take the largest tape and check if it has a pair, then return if it does or
+do not return/return that it only has one if it doesn't. We do not want to
+return a pair if the distance if it is not relevant.
+"""
+
+# tape_list: a list of slanted bounding boxes
+def check_largest_tape(box_list):
+    max_height = -float("inf")
+    max_box = None
+    for box in box_list:
+        top = box[0] # Top point in the box array of four points 
+        bottom = box[2]
+        height = top[1]-bottom[1]
+        if(height > max_height):
+            max_height = height
+            max_box = box
+    
+    inside_angle = findInsideAngle(box[1], box[2], (box[2][0]+0.0001, box[2][1]))
+    return max_box, max_height, inside_angle
+
+# We want to find if a pair exists to the max box and return it. Other contours don't matter
+# As we want to focus on the main focused tape and not just a random background one.
+def pair_finding(box_list, box, target_height, angle):
+    global HEIGHT_THRESHOLD
+    global ANGLE_THRESHOLD
+    # We want to find if a match exists. It should have similar height and opposite angle.
+    possible_matches = []
+    target_angle = (math.pi/2)-angle # In radians
+    for check_box in box_list:
+        check_height = check_box[0][1] - check_box[2][1]
+        check_angle = findInsideAngle(box[1], box[2], (box[2][0]+0.0001, box[2][1]))
+        if(abs(check_height-target_height) < HEIGHT_THRESHOLD and abs(check_angle-target_angle) < ANGLE_THRESHOLD):
+            possible_matches.append(check_box)
+    return possible_matches
+    if len(possible_matches) >= 1:
+        print("FOUND PAIRS") # Then we just take possible_matches[0]
+        return possible_matches[0]
+    else:
+        print("NO PAIRS FOUND")
+        return None
+    
 def right_or_left(box):
     top = box[0]
-    right = box[1]
     bottom = box[2]
-    left = box[3]
+    
     first = min([right, left], key = lambda t:t[1])
     final = (bottom[0]+.1,bottom[1])
     angle = ScoringMetric.slope(first,bottom)
+    
     if angle<0:
         angle = math.pi+angle
     if abs(165.5-angle/math.pi*180)<abs(14.5-angle/math.pi*180):
@@ -15,18 +63,3 @@ def right_or_left(box):
     else:
         return 'l'
 
-def pair_finding(boxes):
-    pairs = []
-    boxes.sort(key=lambda x: x[3][0])
-    if len(boxes)==1:
-        return None
-    list_rotations = []
-    for i in boxes:
-        list_rotations.append(right_or_left(i))
-    for i in range(0,len(list_rotations)-1):
-        if list_rotations[i]=='l' and list_rotations[i+1]=='r':
-            pairs.append((boxes[i],boxes[i+1]))
-    if len(pairs) == 0:
-        return None
-    else:
-        return pairs
