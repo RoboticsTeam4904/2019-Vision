c2ddf486481fa8b29c99cea020e18f2163ef85d1
akshar <aksramk@nuevaschool.org>
2019-02-16 15:25:42 -0800

    Initializing new branch

--- vision_cv/ImageAnalysis.py
+++ vision_cv/ImageAnalysis.py
@@ -15,28 +15,27 @@ def imageAnalysis(images, n=0):
     AllBoxes = []
     saveImage = False
     for img in range(0, len(images)):
-	saveImage = False
-	if img == 1 or True:
-	    saveImage = True
-	img = images[img]
-	if saveImage:
-	    Printing.save(img, name="TEST" + str(n))
+        saveImage = False
+        if img == 1 or True:
+            saveImage = True
+        img = images[img]
+        if saveImage:
+            Printing.save(img, name="TEST" + str(n))
         thresh, contours, mask = GetContours.getContours(img)
         if len(contours) == 0:
             print("\t\t no contours")
             return None
-        contours, mask, boxes, box_scores=getBoxes(contours, mask, img)
+        contours, mask, boxes, box_scores = getBoxes(contours, mask, img)
         # Now we draw boxes;
-        box_scores=sorted(box_scores, key=lambda x: x[1])[::-1]
+        box_scores = sorted(box_scores, key=lambda x: x[1])[::-1]
 
-        box_scores_filtered=[
+        box_scores_filtered = [
             s for s in box_scores if s[1] >= config.MIN_THRESHOLD]
-        boxes_filtered=[s[0]
-            for s in box_scores if s[1] >= config.MIN_THRESHOLD]
+        boxes_filtered = [s[0]
+                          for s in box_scores if s[1] >= config.MIN_THRESHOLD]
 
-
-        box_scores=box_scores_filtered  # Final scores for each contour
-        boxes=boxes_filtered
+        box_scores = box_scores_filtered  # Final scores for each contour
+        boxes = boxes_filtered
 
         # DrawImage.drawPairs(boxes)
         DrawImage.drawBoxes(box_scores, mask)
@@ -69,7 +68,6 @@ def imageAnalysis(images, n=0):
         # cv2.circle(mask, center_point, 3, (255,255,255), thickness=-1)
         # #cv2.rectangle(mask, (x,y), (x+w,y+h), (255,255,255))
 
-
         if not config.LiveImage:
             cv2.imshow("Threshold", thresh)
             cv2.imshow("Image", img)
@@ -81,28 +79,30 @@ def imageAnalysis(images, n=0):
     if(config.NUM_CAMERAS == 2):
         print(GetAngle.getBeta(AllBoxes))
 
+
 def findDistance(contours):
     if len(contours) > 1:
-        dist1=GetDistance.getDistance(cv2.boundingRect(contours[0]))
-        dist2=GetDistance.getDistance(cv2.boundingRect(contours[1]))
-        averaged_distance=(dist1 + dist2)/2
+        dist1 = GetDistance.getDistance(cv2.boundingRect(contours[0]))
+        dist2 = GetDistance.getDistance(cv2.boundingRect(contours[1]))
+        averaged_distance = (dist1 + dist2)/2
         print("IN FEET: ", (averaged_distance/(25.4 * 12)))
         return averaged_distance  # in millimeters
     else:
         print('NOT ENOUGH CONTOURS FOR DISTANCE - - - - - -  -- - -')
 
-def getBoxes(contours, mask, img):
-    box_scores=[]
-    boxes=[]
-    for i in range(len(contours)):
-        rect=cv2.minAreaRect(contours[i])
-        if(cv2.__version__[0] == "2"):
-            box=cv2.cv.BoxPoints(rect)
-        else:
-            box=cv2.boxPoints(rect)
 
-        box=np.int0(box)
-        points, contour_score=ScoringMetric.score(
+def getBoxes(contours, mask, img):
+    box_scores = []
+    boxes = []
+    for i in range(len(contours)):
+        rect = cv2.minAreaRect(contours[i])
+        if(cv2.__version__[0] == "2"):
+            box = cv2.cv.BoxPoints(rect)
+        else:
+            box = cv2.boxPoints(rect)
+
+        box = np.int0(box)
+        points, contour_score = ScoringMetric.score(
             box, contours[i], img, config.WEIGHTS)
         # Array with all of the boxes with the format (t, r, b, l) for pair finding
         boxes.append(points)
