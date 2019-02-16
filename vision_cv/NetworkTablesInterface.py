4d5d3135f1160c424508b3c75cc562e65d486ee0
yashn35 <yasnara04@gmail.com>
2019-02-15 18:18:20 -0800

    Intermediate commit

--- vision_cv/NetworkTablesInterface.py
+++ vision_cv/NetworkTablesInterface.py
@@ -7,5 +7,10 @@ table = NetworkTables.getTable("Vision")
 table.setUpdateRate(config.networkSpeed)
 
 def send_data(x, y, theta, beta, dist):
+    table.putBoolean('x', x)
+	table.putNumber('y', y)
+	table.putNumber('theta', theta)
+	table.putNumber('beta', beta)
+    table.putNumber('distance', dist)
     # TODO: send the network tables
     pass
