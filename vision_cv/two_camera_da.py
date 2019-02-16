1da1dff53fb038f722100f4d644a5a577dabbd67
bcho04 <brandon.cho@gmail.com>
2019-02-16 15:06:55 -0800

    Programmed math on white board

---
+++ vision_cv/two_camera_da.py
@@ -0,0 +1,53 @@
+import math
+
+between_cameras = 1 # ft
+between_tapes = 1 # ft
+
+# ANGLES TO THE LEFT MUST BE POSITIVE, ANGLES TO THE RIGHT MUST BE NEGATIVE
+def get_theta(theta_11, theta_12, theta_21, r=between_cameras, m=between_tapes):
+    k_2 = (math.pi/2) - theta_21
+    k_3 = math.pi- (math.pi/2 + theta_11) + k_2
+    d_11 = r * (math.sin(k_2) / math.sin(k_3))
+    k_6 = theta_11 + theta_12
+    print("gt k11 inp:", (d_11 * math.sin(k_6)) / m)
+    k_11 = math.asin((d_11 * math.sin(k_6)) / m)
+
+    theta_2 = math.pi - k_11 - k_6
+    theta_1 = theta_11 + (math.pi / 2)
+    p = math.sqrt(d_11**2 + (r**2 / 4) - r * d_11 * math.cos(theta_1))
+    print("gt t31 inp:",round(d_11 * math.sin(theta_1) / (p + 0.001), 7)) #maybe don't add 0.001?
+    theta_31 = math.asin(round(d_11 * math.sin(theta_1) / (p + 0.001), 7)) #maybe don't add 0.001?
+    phi = theta_2 - (math.pi - theta_1 - theta_31)
+    D = math.sqrt((m**2 / 4) + p**2 - m * p * math.cos(phi))
+    theta_32 = math.asin((m * math.sin(phi)) / (2 * D + 0.001)) #maybe don't add 0.001?
+
+    theta_3 = theta_31 + theta_32
+    theta = (math.pi / 2) - theta_3
+    return theta # Positive means turn left, negative means turn right
+
+def get_distance(theta_11, theta_12, theta_21, r=between_cameras, m=between_tapes):
+    k_2 = (math.pi/2) - theta_21
+    k_3 = math.pi- (math.pi/2 + theta_11) + k_2
+    d_11 = r * (math.sin(k_2) / math.sin(k_3))
+    k_6 = theta_11 + theta_12
+    print("gd k11 inp:", (d_11 * math.sin(k_6)) / m)
+    k_11 = math.asin((d_11 * math.sin(k_6)) / m)
+
+    theta_2 = math.pi - k_11 - k_6
+    theta_1 = theta_11 + (math.pi / 2)
+    p = math.sqrt(d_11**2 + (r**2 / 4) - r * d_11 * math.cos(theta_1))
+    print("gd t31 inp:",round(d_11 * math.sin(theta_1) / (p + 0.001), 7)) #maybe don't add 0.001?
+    theta_31 = math.asin(round(d_11 * math.sin(theta_1) / (p + 0.001), 7)) #maybe don't add 0.001?
+    phi = theta_2 - (math.pi - theta_1 - theta_31)
+    D = math.sqrt((m**2 / 4) + p**2 - m * p * math.cos(phi))
+    
+    return D # Positive means turn left, negative means turn right
+
+#print(get_theta(math.pi/18, -math.pi/9, 4*math.pi/18, r=1, m=1) * (180/math.pi))
+#print(get_distance(math.pi/18, -math.pi/9, 4*math.pi/18, r=1, m=1))
+
+#print(get_theta(0, -math.pi/4, math.pi/4, r=1, m=1) * (180/math.pi))
+#print(get_distance(0, -math.pi/4, math.pi/4, r=1, m=1))
+
+print(get_theta(math.pi/12, -math.pi/6, -math.pi/12, r=1, m=1) * (180/math.pi))
+print(get_distance(math.pi/12, -math.pi/2, -math.pi/12, r=1, m=1))
