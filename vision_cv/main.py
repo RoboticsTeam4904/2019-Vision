77ee9a2839772f6cabfdac9543f56a549867467b
Yash Narayan <yasnara04@gmail.com>
2019-02-16 23:16:10 +0000

    Intermediate commit

--- vision_cv/main.py
+++ vision_cv/main.py
@@ -8,16 +8,8 @@ import AutoCalibrate
 if(__name__ == "__main__"):
     if config.LiveImage:
         frame_num = 0
-<<<<<<< HEAD
-        print("Camera 0: " + AutoCalibrate.calibrate(0, 1, 100, config.WEIGHTS))
-        print("Camera 1: " + AutoCalibrate.calibrate(1, 1, 100, config.WEIGHTS))
-=======
         print("Camera 0: " + AutoCalibrate.calibrate(0, config.MIN_EXPOSURE, config.MAX_EXPOSURE, config.WEIGHTS))
         print("Camera 1: " + AutoCalibrate.calibrate(1, config.MIN_EXPOSURE, config.MAX_EXPOSURE, config.WEIGHTS))
-        # #WebCam.set(exposure=10)
-        WebCam.set(port=1, exposure=sys.argv[1])
-        WebCam.set(port=0, exposure=sys.argv[1])
->>>>>>> 4b1c74326d9d5a80ea08910292ee15fbca911604
         while True:
             frame_num += 1
             imgs = WebCam.getImages()
