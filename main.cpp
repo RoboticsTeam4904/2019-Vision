a428051c1c7534f980da00d4a5e237bd28da867f
Codadillo <leoconr@nuevaschool.org>
2019-02-18 12:46:23 -0800

    Added fps measuring option

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+#include <chrono>
 #include <iostream>
 #include <vector>
 #include <opencv2/opencv.hpp>
-#include "Config.h"
 #include "AngleFinder.h"
+#include "Config.h"
+
+unsigned long timeStart = 0;
 
 int main() {
     grip::GripPipeline pipeline = grip::GripPipeline();
@@ -10,12 +13,23 @@ int main() {
     cv::Mat buf;
     double angle;
     while (true) {
+        if (DEBUG)
+            timeStart = std::chrono::duration_cast<std::chrono::milliseconds>
+                (std::chrono::system_clock::now().time_since_epoch()).count();
+
         if (camera.read(buf)) {
             angle = AngleFinder::tapeAngle(buf, pipeline);
             std::cout << angle << std::endl;
         } else {
             std::cout << "Camera ded" << std::endl;
         }
+
+        if (DEBUG)
+            std::cout << "Time per frame: " 
+                << std::chrono::duration_cast<std::chrono::milliseconds>
+                    (std::chrono::system_clock::now().time_since_epoch())
+                        .count() - timeStart
+                << std::endl;
     }
     return 0;
 }
