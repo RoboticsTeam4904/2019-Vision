d060739913da8baa78c2a8bf2c1fb36b0ca13a91
yashn35 <yasnara04@gmail.com>
2019-02-18 11:20:52 -0800

    Inter commit

--- vision_cv/TwoCameraMeasurementConsolidation.py
+++ vision_cv/TwoCameraMeasurementConsolidation.py
@@ -1,2 +1,12 @@
+
 def twoCameraMeasurementConslidation(leftMeasurements, rightMeasurements):
     #TODO implement 
+
+def finalDistance(x, y):
+    return pass
+
+def final theta(x, y):
+    return pass
+
+def getXandY(dist, finalTheta, finalBeta):
+    return (x, y)
