7651ed98475dbda72d5c3fefb7d8e20b12fa6800
akshar <aksramk@nuevaschool.org>
2019-02-19 13:36:49 -0800

    Initializing branch

---
+++ src/Config.hpp
@@ -0,0 +1,17 @@
+// Image Capture Settings
+#define live_image = False
+#define two_cameras True
+
+// Logging/Debug settings
+
+#define display True
+#define save False
+#define save_in_folder True
+#define debug True
+#define extra_debug False  // step through each contour in the webcam by turning on extra_debug
+
+// Network Output Settings
+#define network_tables True
+#define can False
+#define sockets False
+
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <chrono>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+
