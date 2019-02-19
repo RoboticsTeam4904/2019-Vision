3dc060480b2da8ffa3c7c7eb9e16fe48ad60cd42
Codadillo <leoconr@nuevaschool.org>
2019-02-18 16:38:05 -0800

    hpp > h

--- AngleFinder.h
+++
@@ -1,6 +0,0 @@
-#include <opencv2/opencv.hpp>
-#include "GripPipeline.h"
-
-namespace AngleFinder {
-    double tapeAngle(cv::Mat &mat, grip::GripPipeline);
-}
--- Config.h
+++
@@ -1,4 +0,0 @@
-#define DEBUG false
-#define USE_NETWORKTABLES false
-#define NETWORKTABLES_PORT 1735
-#define TEAM_NUMBER 4904
