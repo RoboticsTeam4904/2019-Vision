b87d68332be5461eb9e1f052579b7bf1a5adb06b
Codadillo <leoconr@nuevaschool.org>
2019-02-18 16:37:20 -0800

    🅱️

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <chrono>
 #include <iostream>
-#include <vector>
 #include <opencv2/opencv.hpp>
+#include <vector>
 #include "AngleFinder.h"
 #include "Config.h"
 #include "networktables/NetworkTable.h"
@@ -11,12 +11,18 @@
 unsigned long timeStart = 0;
 
 int main() {
+    // Inform the user of config
+    std::cout 
+        << "Running with DEBUG=" << DEBUG 
+        << " USE_NETWORKTABLES=" << USE_NETWORKTABLES 
+        << " NETWORKTABLES_PORT=" << NETWORKTABLES_PORT 
+        << " TEAM_NUMBER=" << TEAM_NUMBER
+        << std::endl;
+
     // Initialize networktables
     std::cout << "Initializing networktables using port " << NETWORKTABLES_PORT << " and team number " << TEAM_NUMBER << std::endl;
-    nt::NetworkTableInstance inst = nt::NetworkTableInstace::GetDefault();
+    nt::NetworkTableInstance inst = nt::NetworkTableInstance::GetDefault();
     inst.StartClientTeam(TEAM_NUMBER, NETWORKTABLES_PORT);
-    while (!inst.isConnected())
-        continue;
     nt::NetworkTableEntry tapeAngleEntry = inst.GetEntry("Vision/tapeAngle");
 
     // Start finding angles
@@ -26,13 +32,12 @@ int main() {
     double angle;
     while (true) {
         if (DEBUG)
-            timeStart = std::chrono::duration_cast<std::chrono::milliseconds>
-                (std::chrono::system_clock::now().time_since_epoch()).count();
+            timeStart = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
         if (camera.read(buf)) {
             angle = AngleFinder::tapeAngle(buf, pipeline);
             if (USE_NETWORKTABLES)
-                tapeAngleEntry.setDouble(angle);
+                tapeAngleEntry.SetDouble(angle);
             if (DEBUG)
                 std::cout << angle << std::endl;
         } else {
@@ -40,11 +45,11 @@ int main() {
         }
 
         if (DEBUG)
-            std::cout << "Time per frame: " 
-                << std::chrono::duration_cast<std::chrono::milliseconds>
-                    (std::chrono::system_clock::now().time_since_epoch())
-                        .count() - timeStart
-                << std::endl;
+            std::cout << "Time per frame: "
+                      << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())
+                                 .count() -
+                             timeStart
+                      << std::endl;
     }
     return 0;
 }
