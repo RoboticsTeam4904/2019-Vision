e237bbc8fb1d534ccc59e4ef7b4ef372b009ccfb
Codadillo <leoconr@nuevaschool.org>
2019-02-19 09:21:30 -0800

    I am competent thank you very much

---
+++ AngleFinder.hpp
@@ -0,0 +1,6 @@
+#include <opencv2/opencv.hpp>
+#include "GripPipeline.hpp"
+
+namespace AngleFinder {
+    double tapeAngle(cv::Mat &mat, grip::GripPipeline);
+}
---
+++ Config.hpp
@@ -0,0 +1,4 @@
+#define DEBUG false
+#define USE_NETWORKTABLES false
+#define NETWORKTABLES_PORT 1735
+#define TEAM_NUMBER 4904
