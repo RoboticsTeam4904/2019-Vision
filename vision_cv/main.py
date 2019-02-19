36623934ab708359f027243f8d7aaa1df65887a3
akshar <aksramk@nuevaschool.org>
2019-02-19 10:38:42 -0800

    Fixing main.py errors

--- vision_cv/main.py
+++ vision_cv/main.py
@@ -18,31 +18,41 @@ if(__name__ == "__main__"):
         WebCam.set(port=Constants.RIGHT_CAMERA_PORT, exposure=sys.argv[1])
         # print("Camera 0: " + AutoCalibrate.calibrate(0, config.MIN_EXPOSURE, config.MAX_EXPOSURE, config.WEIGHTS))
         # print("Camera 1: " + AutoCalibrate.calibrate(1, config.MIN_EXPOSURE, config.MAX_EXPOSURE, config.WEIGHTS))
-        while True: #"while True" code can only run if LiveImage from config.py is set to True
+        while True:  # "while True" code can only run if LiveImage from config.py is set to True
             frame_num += 1
             leftImage, rightImage = WebCam.getImages()
-            leftMeasurements = ImageAnalysis.imageAnalysis(leftImage) #leftMesaurements is a tuple of isVisible, left camera distance, left camera theta
-            rightMeasurements = ImageAnalysis.imageAnalysis(rightImage) #rightMesaurements is a tuple of isVisible boolean, right camera distance, right camera theta
-            if leftMeasurements[0] and rightMeasurements[0] == False:
-                isVisible = False 
+            # leftMesaurements is a tuple of isVisible, left camera distance, left camera theta
+            leftMeasurements = ImageAnalysis.imageAnalysis(leftImage)
+            # rightMesaurements is a tuple of isVisible boolean, right camera distance, right camera theta
+            rightMeasurements = ImageAnalysis.imageAnalysis(rightImage)
+            if (leftMeasurements[0][0] and rightMeasurements[0][0] is False) or (leftMeasurements[1][0] and rightMeasurements[1][0] is False):
+                isVisible = False
                 continue
-            beta = GetAngle.getBeta(leftMeasurements[0][0:2], leftMeasurements[1][0:2], rightMeasurements[0][0:2], rightMeasurements[1][0:2]) #Get's beta
-	    print("THIS IS BETA", beta)
-            #Get's final theta and distance from center of the tape to center of the robot
+            beta = GetAngle.getBeta(leftMeasurements[0][0:2], leftMeasurements[1][0:2],
+                                    rightMeasurements[0][0:2], rightMeasurements[1][0:2])  # Get's beta
+            print("\t \t \t BETA (In degrees): " +  str(beta))
+
+            if not leftMeasurements[0][0] and not rightMeasurements[1][0]:
+                continue
+            # Get's final theta and distance from center of the tape to center of the robot
             if(leftMeasurements[0] or rightMeasurements[0]):
-	        finalTheta, finalDistance = TwoCameraMeasurementConsolidation.finalDistanceTheta(leftMeasurements[0][3], 
-	        rightMeasurements[1][3], leftMeasurements[0][2], rightMeasurements[1][2]) 
-	        if beta:
-                    x, y = TwoCameraMeasurementConsolidation.getXandY(finalTheta, finalDistance, beta) #returns x and y coordinate from center of tape to center of robot
-                    print("X COORDINATE", x)
-		    print("Y COORDINATE", y)
-                else:
+                finalTheta, finalDistance = TwoCameraMeasurementConsolidation.finalDistanceTheta(leftMeasurements[0][3],
+                                                                                                 rightMeasurements[1][3], leftMeasurements[0][2], rightMeasurements[1][2])
+                if not beta:
                     x, y = 0, 0
-                    print ("CAN'T PASS IN BETA OR FINAL DISTANCE AND THETA")
-            #finalTheta finalDistance is the final theta and distance from the center of the robot to the center of the tape.
-            
+                    print ("\t \t \t UNABLE TO FIND BETA")
+
+                else:
+                    x, y = TwoCameraMeasurementConsolidation.getXandY(
+                        finalTheta, finalDistance, beta)
+                    print("X COORDINATE", x)
+                    print("Y COORDINATE", y)
+                    # returns x and y coordinate from center of tape to center of robot
+                    
+            # finalTheta finalDistance is the final theta and distance from the center of the robot to the center of the tape.
+
             #NetworkTablesInterface.send_data(x, y, finalTheta, beta, finalDistance, frame_num)
-            print("FINAL THETA: " +  str(finalTheta))
+            print("FINAL THETA: " + str(finalTheta))
             print("FINAL DISTANCE: " + str(finalDistance))
     else:
         # Taking an image from folder of TestImages
