391412fcae8df397b20a0fbaaee945382b8e954a
akshar <aksramk@nuevaschool.org>
2019-02-20 11:37:52 -0800

    Fixing syntax and other minor errors

--- src/GetBoxes.cpp
+++ src/GetBoxes.cpp
@@ -9,7 +9,7 @@
 #include <cstdlib>
 //This function takes in an image, finds all of the contours, and filters them with the scoringMetric evaluation.
 
-std::vector<std::vector<cv::Point>> getBoxes::getBoxes(cv::Mat &img)
+std::vector<std::vector<cv::Point>> getBoxes::getBoxes(&img)
 {
     pipeline.Process(img);
     std::vector<std::vector<cv::Point>> contours = *pipeline.GetFilterContoursOutput();
@@ -28,10 +28,7 @@ std::vector<std::vector<cv::Point>> getBoxes::getBoxes(cv::Mat &img)
 }
 
 //This function calles all of the scoring evaluations, returning a final score for a given box of how likely it is to be a tape
-std::optional<std::vector<cv::Point>> getBoxes::scoringMetric(std::vector<cv::Point> &contour,
-                                                              int HW_RATIO = 5, int AREA_RATIO = 1, int FILLED_AREA = 10,
-                                                              int ROTATION_ANGLE_INFUNC = 1,
-                                                              int ROTATION_ANGLE_OUTFUNC = 1, int THRESHOLD = 13)
+std::optional<std::vector<cv::Point>> getBoxes::scoringMetric(&contour)
 {
     double score = 0;
     cv::Point top;
@@ -64,7 +61,7 @@ std::optional<std::vector<cv::Point>> getBoxes::scoringMetric(std::vector<cv::Po
     score += scoring_rotation_angle(right, bottom, ROTATION_ANGLE_INFUNC) * ROTATION_ANGLE_OUTFUNC;
     score += scoring_filled_value(contour, box) * FILLED_AREA;
 
-    if (score > THRESHOLD)
+    if (score > MIN_THRESHOLD)
     {
         //We return a box if its score is above a certain threshold
         return box;
@@ -75,20 +72,20 @@ std::optional<std::vector<cv::Point>> getBoxes::scoringMetric(std::vector<cv::Po
 }
 
 //This function gives a box a score based on how accurate the ratio of the length of the sides is
-double getBoxes::scoring_side_ratio(double width, double height)
+double getBoxes::scoring_side_ratio(width, height)
 {
     if (width == 0 || height == 0)
         return 0;
     double target_ratio = 2.75; //Ratio of length and width of slanted bounding box of tape
-    double score = std::max(1 / ((target_ratio - width / height) ^ 2 + 1),
-                            1 / ((target_ratio - height / width) ^ 2 + 1));
+    double score = std::max(1 / (pow(target_ratio - width / height,2) + 1),
+                            1 / (pow(target_ratio - height / width,2) + 1));
     return score;
 }
 
 //This function gives a score based on the ratio of the area of the slanted and straight bounding box.
-double getBoxes::scoring_area_ratio(double width, double height, std::vector<cv::Point> &points)
+double getBoxes::scoring_area_ratio(width, height, &points)
 {
-    target_ratio = 0.5698;
+    double target_ratio = 0.5698;
     if (width == 0 || height == 0)
         return 0;
     cv::Point left = points[0];
@@ -101,12 +98,12 @@ double getBoxes::scoring_area_ratio(double width, double height, std::vector<cv:
     {
         return 0;
     }
-    double score = 1 / ((target_ratio - (area_slanted / area_straight)) ^ 2 + 1;
+    double score = 1 / (pow(target_ratio - (area_slanted / area_straight), 2) + 1;
     return score;
 }
 
 //This function finds how rotated the contour is: optimally, 75.5 degrees or 14.5 degrees.
-double getBoxes::scoring_rotation_angle(cv::Point &right, cv::Point &bottom, double weight)
+double getBoxes::scoring_rotation_angle(&right, &bottom, weight)
 {
     double rotationAngle = getBoxes::angle(right, bottom);
     rotationAngle = rotationAngle / M_PI * 180;
@@ -114,30 +111,33 @@ double getBoxes::scoring_rotation_angle(cv::Point &right, cv::Point &bottom, dou
     return -num / (num + weight) + 1
 }
 //This function finds how much of the contour is legitimately on the slanted bounding box that it was fit -- the optimal contour, like the tape, would have almost all of it's points on (or crossing) the box.
-double getBoxes::scoring_filled_value(std::vector<cv::Point> contour, std::vector<cv::Point> box)
+double getBoxes::scoring_filled_value(&contour, &box)
 {
-    int max_y = box[3].y;
-    int min_y = box[2].y;
-    int min_x = box[0].x;
-    int max_x = box[1].x;
+    std::vector<std::vector<cv::Point>> contour1 = contour;
+    std::vector<std::vector<cv::Point>> box1 = box;
+    int max_y = box1[3].y;
+    int min_y = box1[2].y;
+    int min_x = box1[0].x;
+    int max_x = box1[1].x;
     for (k = 0; k < 4; ++k)
     {
-        box[k].x -= min_x;
-        box[k].y -= min.y;
+        box1[k].x -= min_x;
+        box1[k].y -= min.y;
     }
 
-    for (c = 0; c < contour.size(); ++k)
+    for (c = 0; c < contour1.size(); ++k)
     {
-        contour[c].x -= min_x;
-        contour[c].y -= min.y;
+        contour1[c].x -= min_x;
+        contour1[c].y -= min.y;
     }
     std::vector<std::vector<cv::Point>> contours;
     std::vector<std::vector<cv::Point>> boxes;
-    boxes.insert(box);
-    contours.insert(contour)
+    boxes.insert(box1);
+    contours.insert(contour1)
     cv::Mat dst = cv::Mat::zeros(max_y-min_y, max_x-min_x, CV_8UC1);
     cv::drawContours(dst, contours, -1, 128, thickness=-1);
     cv::drawContours(dst, boxes, -1, 255, thickness=-1);
+
     
 }
 
