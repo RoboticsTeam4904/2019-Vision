799f9890a4ec27c71a98a048460de5aaaad07ddc
Codadillo <leoconr@nuevaschool.org>
2019-02-20 18:04:09 -0800

    There's still so much more to do. If you're reading this Akshar, be scared of when we talk next

--- src/GetDistance.cpp
+++ src/GetDistance.cpp
@@ -11,8 +11,7 @@ std::optional<double> GetDistance::getDistanceToWall(std::vector<cv::Point> &box
     cv::Point bottom = box[2];
     double heightPixels = top.y - bottom.y;
     double distanceConstant = cos(gamma) * Config::DISTANCE_CONSTANT;
-    double distanceToObject =  distanceConstant / heightPixels * 0.03937;
-    return distanceToObject; //Return distance in inches
+    return distanceConstant / heightPixels * 0.03937; // TODO: I have no clue what 0.03937 is but make it a constant
 }
 
 std::optional<double> GetDistance::getDistanceToTape(std::vector<cv::Point> &box, double theta, double gamma)
@@ -21,7 +20,5 @@ std::optional<double> GetDistance::getDistanceToTape(std::vector<cv::Point> &box
     cv::Point bottom = box[2];
     double heightPixels = top.y - bottom.y;
     double distanceConstant = cos(gamma) * Config::DISTANCE_CONSTANT;
-    double distance = distanceConstant / heightPixels * 0.03937;
-    distance /= cos(theta);
-    return distance;
+    return (distanceConstant / heightPixels * 0.03937) / cos(theta); // TODO: I have no clue what 0.03937 is but make it a constant
 }
--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main() {
             << " NETWORKTABLES_PORT=" << Config::NETWORKTABLES_PORT
             << " TEAM_NUMBER=" << Config::TEAM_NUMBER
             << std::endl;
+
     grip::GripPipeline pipeline = grip::GripPipeline();
     cv::VideoCapture leftCamera(Config::LEFT_CAMERA_PORT);
     cv::VideoCapture rightCamera(Config::RIGHT_CAMERA_PORT);
@@ -24,16 +25,14 @@ int main() {
             timeStart = std::chrono::duration_cast<std::chrono::milliseconds>(
                     std::chrono::system_clock::now().time_since_epoch()).count();
 
-        if (leftCamera.read(leftImg)) {
+        if (leftCamera.read(leftImg))
             std::vector<std::vector<cv::Point>> leftBoxes = GetBoxes::getBoxes(leftImg, pipeline);
-        } else {
+        else
             std::cout << "Unable to get image from camera with port " << Config::LEFT_CAMERA_PORT << std::endl;
-        }
 
-        if (rightCamera.read(rightImg)) {
+        if (rightCamera.read(rightImg))
             std::vector<std::vector<cv::Point>> rightBoxes = GetBoxes::getBoxes(rightImg, pipeline);
-        } else {
+        else
             std::cout << "Unable to get image from camera with port " << Config::RIGHT_CAMERA_PORT << std::endl;
-        }
     }
 }
