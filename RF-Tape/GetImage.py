6540c546808367ecaf069d6931ed2c57ae801b8d
Osher Lerner <oshlern@nuevaschool.org>
2019-02-23 21:34:00 -0800

    converting GetImage to Object-oriented cameras

--- RF-Tape/GetImage.py
+++ RF-Tape/GetImage.py
@@ -1,13 +1,29 @@
-import WebCam, Printing
+import WebCam, Printing, config, Constants
 
-def s():  # Take a picture from the webcam and save.
-    left_image, right_image = WebCam.getImages()
-    Printing.save_pair(right_image, "test")
+if config.two_cameras:
+    left_camera = WebCam.Camera(Constants.LEFT_CAMERA_PORT)
+    right_camera = WebCam.Camera(Constants.RIGHT_CAMERA_PORT)
 
-def e(port=0, exposure=0.1): # Set exposure on LINUX ONLY.
-    WebCam.set(port=port, exposure=exposure)
+    left_camera.configure(exposure=Constants.exposure, resolution=Constants.resolution)
+    right_camera.configure(exposure=Constants.exposure, resolution=Constants.resolution)
 
-def d(): # Display images
-    left_image, right_image = WebCam.getImages()
-    Printing.display(left_image, "left")
-    Printing.display(left_image, "right")
+    def s():  # Take a picture from both webcams and save.
+        left_image, right_image = left_camera.getImage(), right_camera.getImage()
+        Printing.save_pair(left_image, right_image, "test")
+
+    def d(): # Display images
+        left_image, right_image = WebCam.getImages()
+        Printing.display(left_image, "left")
+        Printing.display(right_image, "right")
+
+    def e(exposure=Constants.exposure): # Set exposure on LINUX ONLY.
+        left_camera.configure(exposure=exposure)
+        right_camera.configure(exposure=exposure)
+
+else:
+    camera = WebCam.Camera(Constants.ONE_CAMERA_PORT)
+    camera.configure(Constants.exposure, Constants.resolution)
+
+
+
+
