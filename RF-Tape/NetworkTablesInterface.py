ce008a5556a7dda2883e56b8a6eb23ba4bc73b4e
Osher Lerner <oshlern@nuevaschool.org>
2019-02-23 16:55:59 -0800

    using network tables as objects

--- RF-Tape/NetworkTablesInterface.py
+++ RF-Tape/NetworkTablesInterface.py
@@ -1,16 +1,26 @@
-from ntworktables import NetworkTables
-import Constants 
+from networktables import NetworkTables
+import Constants
 
 NetworkTables.setTeam(Constants.team)
 NetworkTables.initialize(server=Constants.ip)
-table = NetworkTables.getTable("Vision")
-table.setUpdateRate(Constants.networkSpeed)
 
-def send_data(x, y, theta, beta, dist, frameNum, isVisible=True):
-    table.putBoolean("trustable", isVisible)
-    table.putBoolean('x', x)
-    table.putNumber('y', y)
-    table.putNumber('theta', theta)
-    table.putNumber('beta', beta)
-    table.putNumber('distance', dist)
-    table.putNumber("frameNum", frameNum)
+class Table: # TODO: Later, use two tables, when organizing RF and floor together
+    def __init__(self, name="Vision"):
+        self.table = NetworkTables.getTable(name)
+        self.table.setUpdateRate(Constants.networkSpeed)
+
+    # Take in data as dictionary as to allow sending of partial information
+    def send_data(data): 
+        assert type(data) == dict
+        self.table.putBoolean("visible", data.get("is_visible", False)) # Default "visible" to False
+
+        # if frame_num in data: # only update frame_num if it was passed
+        self.table.putNumber("RF/frame_num", data.get("frame_num", -1))
+        self.table.putNumber("RF/theta", data.get("theta", 0))
+        self.table.putNumber("RF/dist", data.get("dist", 0))
+        self.table.putNumber("RF/beta", data.get("beta", 0))
+        self.table.putNumber("RF/x", data.get("x", 0))
+        self.table.putNumber("RF/y", data.get("y", 0))
+
+    def get_floor_beta(self):
+        return self.table.getNumber("Floor/beta", 0)
