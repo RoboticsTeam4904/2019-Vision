ef1713d37b5bfea89e730e481860cb77476eb96c
Osher Lerner <oshlern@nuevaschool.org>
2019-02-23 16:55:44 -0800

    using cameras as objects

--- RF-Tape/WebCam.py
+++ RF-Tape/WebCam.py
@@ -1,19 +1,45 @@
 import cv2
 import subprocess
-import numpy as np
-from PIL import Image
-import config
-import Constants
-import GetImage
+# import config
+# import Constants
 
-if config.two_cameras:
-    leftCamera = cv2.VideoCapture(Constants.LEFT_CAMERA_PORT)
-    rightCamera = cv2.VideoCapture(Constants.RIGHT_CAMERA_PORT)
-    configureCamera(Constants.LEFT_CAMERA_PORT, Constants.exposure, Constants.resolution)
-    configureCamera(Constants.RIGHT_CAMERA_PORT, Constants.exposure, Constants.resolution)
-else:
-    camera = cv2.VideoCapture(Constants.ONE_CAMERA_PORT)
-    configureCamera(Constants.ONE_CAMERA_PORT, Constants.exposure, Constants.resolution)
+# if config.two_cameras:
+#     leftCamera = cv2.VideoCapture(Constants.LEFT_CAMERA_PORT)
+#     rightCamera = cv2.VideoCapture(Constants.RIGHT_CAMERA_PORT)
+#     configureCamera(Constants.LEFT_CAMERA_PORT, Constants.exposure, Constants.resolution)
+#     configureCamera(Constants.RIGHT_CAMERA_PORT, Constants.exposure, Constants.resolution)
+# else:
+#     camera = cv2.VideoCapture(Constants.ONE_CAMERA_PORT)
+#     configureCamera(Constants.ONE_CAMERA_PORT, Constants.exposure, Constants.resolution)
+
+class Camera:
+    def __init__(self, port, name=None):
+        self.port = port
+        self.camera = cv2.VideoCapture(port)
+
+        if name == None:
+            self.name = "Camera" + str(port)
+        else:
+            assert type(name) == str
+            self.name = name
+
+    def getImage(self):
+        image_found, image = self.camera.read()
+        if not image_found:
+            raise TypeError("ERROR: {} failed to read image at port {}".format(name, port))
+        return image
+
+    def configure(self, exposure=False, resolution=False, gain=False, contrast=False):
+        settingStr = "/usr/bin/v4l2-ctl -d /dev/video" + str(port)
+        if resolution:
+            settingStr += " --set-fmt-video=width={},height={}".format(resolution[0], resolution[1])
+        if exposure:
+            settingStr += " -c exposure_auto=1 -c exposure_auto_priority=0 -c exposure_absolute={}".format(exposure)
+        if gain:
+            settingStr += " -c gain={}".format(gain)
+        if contrast:
+            settingStr += " -c contrast={}".format(contrast)
+        subprocess.call(settingStr, shell=True)
 
 def getImages(): # This is for two camera
     assert config.two_cameras, "config.two_cameras must be True to read from two cameras"
@@ -31,14 +57,5 @@ def getImage(camera):
         raise Exception("Image reading failed for camera at port {}".format(camera))
     return image
 
-def configureCamera(port, exposure=False, resolution=False, gain=False, contrast=False): # Configures camera (for Linux only) 
-    settingStr = "/usr/bin/v4l2-ctl -d /dev/video" + str(port)
-    if resolution:
-        settingStr += " --set-fmt-video=width={},height={}".format(resolution[0], resolution[1])
-    if exposure:
-        settingStr += " -c exposure_auto=1 -c exposure_auto_priority=0 -c exposure_absolute={}".format(exposure)
-    if gain:
-        settingStr += " -c gain={}".format(gain)
-    if contrast:
-        settingStr += " -c contrast={}".format(contrast)
-    subprocess.call(settingStr, shell=True)
+def configureCamera(port): # Configures camera (for Linux only) 
+    
