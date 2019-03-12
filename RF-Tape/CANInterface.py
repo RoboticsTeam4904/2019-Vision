8842329c06330b5b3217ceec9b336315ce8678e0
Osher Lerner <oshlern@nuevaschool.org>
2019-03-11 17:24:34 -0700

    oop CAN

--- RF-Tape/CANInterface.py
+++ RF-Tape/CANInterface.py
@@ -1,24 +1,19 @@
-import can
+import can # TODO: make dependency list
 import struct
 
-bus = can.Bus(
-    interface="socketcan",
-    channel="idk",
-    receive_own_messages=False)
+class CAN:
+    def __init__(self):
+        self.bus = can.Bus(
+            interface="socketcan",
+            channel="idk",
+            receive_own_messages=False)
 
-def send_data(x, y, theta, beta, dist):
-    values = [
-        # TODO: defaulted to 4 bytes, size subject to bikeshedding
-        struct.pack('i', x),
-        struct.pack('i', y),
-        struct.pack('f', theta),
-        struct.pack('f', beta),
-        struct.pack('i', dist),
-    ]
+    def send_data(self, data):
+        values = [struct.pack('f', data[key]) for key in data] # TODO: defaulted to 4 bytes, size subject to bikeshedding
 
-    payloads = [bytearray(value) for value in values]
+        payloads = [bytearray(value) for value in values]
 
-    for i in range(len(payloads)):
-        # decide on a good arbitration ID later
-        message = can.Message(arbitration_id=0x1100 + i, data=payloads[i])
-        bus.send(message, timeout=0.2)
+        for i in range(len(payloads)):
+            # decide on a good arbitration ID later
+            message = can.Message(arbitration_id=0x1100 + i, data=payloads[i])
+            self.bus.send(message, timeout=0.2)
